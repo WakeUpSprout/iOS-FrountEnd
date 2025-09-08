dab2f3ea63cd033678aa4f64b590beb18b38dc84
leebuyong <dqd001@naver.com>
2025-09-08 16:21:55 +0900

    [Setting] LHLM-2 SwiftUI View 테스트용 설정

---
+++ WakeUpSprout/WakeUpSprout/Presentation/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WakeUpSprout
+//
+//  Created by 이부용 on 9/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("TestText")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
